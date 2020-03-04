6a5555f0284723d6201a04270e18eedd6852b013
Mohamed Abd ELmawgoud <49039674+MohamedAbdElmawgoud@users.noreply.github.com>
2020-03-04 15:17:20 +0200

    add service

---
+++ src/app/admin/service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  constructor() { }
+
+  async get(): any{
+     let yourObserver :any = [];
+    let dataObserval= new Observable  (data=>
+    
+    {
+      yourObserver = data;
+    })
+return yourObserver.toPromise()
+  } 
+}
