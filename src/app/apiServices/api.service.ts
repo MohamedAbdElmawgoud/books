5f1a92c6e40ead366b7755178bd8765156e6e0c9
nader medhet <nadermedhet142@gmail.com>
2020-03-09 12:50:56 +0200

    add all api services

--- src/app/apiServices/api.service.ts
+++ src/app/apiServices/api.service.ts
@@ -3,8 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateService } from '@ngx-translate/core';
 
 const LNGS = {
-  en : 'en',
-  ar : 'ar'
+  en: 'en',
+  ar: 'ar'
 }
 @Injectable({
   providedIn: 'root'
@@ -12,83 +12,162 @@ const LNGS = {
 export class ApiService {
   private url = 'http://backend.q3seo.com/books/public/api/';
   defaultLng;
-  constructor(private httpClient : HttpClient ,private translateService : TranslateService )   {
+  constructor(private httpClient: HttpClient, private translateService: TranslateService) {
     this.defaultLng = this.translateService.defaultLang
-   }
+  }
 
   async getSlider() {
-    
-    let data = (<any> await this.httpClient.get(`${this.url}slider/getAll?all=yes`).toPromise()).data;
 
-    return data.map(ele=>{
+    let data = (<any>await this.httpClient.get(`${this.url}slider/getAll?all=yes`).toPromise()).data;
+
+    return data.map(ele => {
       return {
-        image : ele.image ,
-        caption : this.defaultLng == LNGS.en ? ele.caption : ele.captionAr
+        id: ele.id,
+
+        image: ele.image,
+        caption: this.defaultLng == LNGS.en ? ele.caption : ele.captionAr
       }
     });
   }
-  async getAboutUs(){
-    let data = (<any> await this.httpClient.get(`${this.url}about-us/getAll?all=yes`).toPromise()).data;
+  async getAboutUs() {
+    let data = (<any>await this.httpClient.get(`${this.url}about-us/getAll?all=yes`).toPromise()).data;
 
-    return data.map(ele=>{
+    return data.map(ele => {
       return {
-        header : this.defaultLng == LNGS.en ? ele.header : ele.headerAr ,
-        content : this.defaultLng == LNGS.en ? ele.content : ele.contentAr
-      }
-    });
-  }
+        id: ele.id,
 
-  async getServices(){
-    let data = (<any> await this.httpClient.get(`${this.url}service/getAll?all=yes`).toPromise()).data;
-
-    return data.map(ele=>{
-      return {
-        image : ele.image,
-        caption : this.defaultLng == LNGS.en ? ele.caption : ele.captionAr,
-        name : this.defaultLng == LNGS.en ? ele.name : ele.nameAr
+        header: this.defaultLng == LNGS.en ? ele.header : ele.headerAr,
+        content: this.defaultLng == LNGS.en ? ele.content : ele.contentAr
       }
     });
   }
 
-  async getFaqs(){
-    let data = (<any> await this.httpClient.get(`${this.url}faq/getAll?all=yes`).toPromise()).data;
+  async getServices() {
+    let data = (<any>await this.httpClient.get(`${this.url}service/getAll?all=yes`).toPromise()).data;
 
-    return data.map(ele=>{
+    return data.map(ele => {
       return {
-        header : this.defaultLng == LNGS.en ? ele.header : ele.headerAr ,
-        content : this.defaultLng == LNGS.en ? ele.content : ele.contentAr
+        id: ele.id,
+
+        image: ele.image,
+        caption: this.defaultLng == LNGS.en ? ele.caption : ele.captionAr,
+        name: this.defaultLng == LNGS.en ? ele.name : ele.nameAr
       }
     });
   }
-  async getShop(){
 
+  async getFaqs() {
+    let data = (<any>await this.httpClient.get(`${this.url}faq/getAll?all=yes`).toPromise()).data;
+
+    return data.map(ele => {
+      return {
+        id: ele.id,
+
+        header: this.defaultLng == LNGS.en ? ele.header : ele.headerAr,
+        content: this.defaultLng == LNGS.en ? ele.content : ele.contentAr
+      }
+    });
   }
-  async getExtras(){
-    let data = (<any> await this.httpClient.get(`${this.url}extras/getAll?all=yes`).toPromise()).data;
+  async getShop() {
+    let data = (<any>await this.httpClient.get(`${this.url}item/getAll?all=yes`).toPromise()).data;
 
-    return data.map(ele=>{
+    return data.map(ele => {
+      return {
+        name: this.defaultLng == LNGS.en ? ele.name : ele.nameAr,
+        description: this.defaultLng == LNGS.en ? ele.description : ele.descriptionAr,
+        price: ele.price,
+        id: ele.id
+      }
+    });
+  }
+  async getExtras() {
+    let data = (<any>await this.httpClient.get(`${this.url}extras/getAll?all=yes`).toPromise()).data;
+
+    return data.map(ele => {
+      return {
+        name: this.defaultLng == LNGS.en ? ele.name : ele.nameAr,
+        price: ele.price
+      }
+    });
+  }
+  async getCategories() {
+    let data = (<any>await this.httpClient.get(`${this.url}category/getAll?all=yes`).toPromise()).data;
+
+    return data.map(ele => {
+      return {
+        id: ele.id,
+        name: this.defaultLng == LNGS.en ? ele.name : ele.nameAr,
+      }
+    });
+  }
+  async getOrders() {
+    let data = (<any>await this.httpClient.get(`${this.url}order/getAll?all=yes`, {
+      headers: {
+        Authorization: localStorage.getItem('token')
+      }
+    }).toPromise()).data;
+
+    return data.map(ele => {
       return ele
-    });
+    })
   }
-  async getCategories(){
+  async Purchase(params: { amount: number }) {
+    let data = (<any>await this.httpClient.post(`${this.url}users/purchase`, params, {
+      headers: {
+        Authorization: localStorage.getItem('token')
+      }
+    }).toPromise());
 
+    return data
   }
-  async getOrders(){
+  async order(params: {
+    "comment": string,
+    "itemId": number,
+    "totalAmount": number,
+    "extras": number[],
+    "links": string[],
+    "keywords": string[],
+    "article": string,
+    "quantity": number
+  }) {
+    let data = (<any>await this.httpClient.post(`${this.url}order/create`, params, {
+      headers: {
+        Authorization: localStorage.getItem('token')
+      }
+    }).toPromise());
 
+    return data
   }
-  async Purchase(){
-    
+
+  async login(params: {
+    email: string,
+    password: string
+  }) {
+    let data = (<any>await this.httpClient.post(`${this.url}users/login`, params).toPromise());
+    localStorage.setItem('user', JSON.stringify(data.user[0]));
+    localStorage.setItem('token', data.token);
   }
-  async order(){
 
+  async register(params: {
+    email: string,
+    password: string,
+    name
+  }) {
+    let data = (<any>await this.httpClient.post(`${this.url}users/register`, params).toPromise());
+
+    localStorage.setItem('user', JSON.stringify(data.user[0]));
+    localStorage.setItem('token', data.token);
   }
 
-  async login(){
+  async contact(params: {
+    'name': string,
+    'phone': string,
+    "mail": string,
+    "message": string
 
-  }
-
-  async register(){
-
+  }) {
+    let data = (<any>await this.httpClient.post(`${this.url}contact-us/create`, params).toPromise());
+    return data;
   }
 
 
