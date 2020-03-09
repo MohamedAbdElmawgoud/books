ece56993d7796a719c54ae1d54700fe2c16b971a
nader medhet <nadermedhet142@gmail.com>
2020-03-09 21:06:12 +0200

    add home ui with server

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,95 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from "src/app/admin/service.service";
-import {TranslateService} from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 import { ApiService } from '../apiServices/api.service';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2'
+declare const $;
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-   slider;
-   contact;
-  
-  constructor(public Service: ServiceService,public translate:  TranslateService ,private apiService : ApiService ) { 
-    const  currentLanguage  =  translate.getBrowserLang();
+  slider;
+  services;
+
+  contactForm = new FormGroup({
+    'name': new FormControl('', [Validators.required]),
+    'phone': new FormControl('', [Validators.required]),
+    "mail": new FormControl('', [Validators.required , Validators.email]),
+    "message": new FormControl('', [Validators.required]),
+
+
+  });
+
+  constructor(public Service: ServiceService, public translate: TranslateService, private apiService: ApiService) {
+    const currentLanguage = translate.getBrowserLang();
     translate.setDefaultLang(currentLanguage);
     translate.use('currentLanguage');
   }
 
   async ngOnInit() {
-    this.slider = await this.apiService.getFaqs();
-  // this.contact = await this.apiService.contact();
-  };
-  
-  Translate(type: string){
-    
-    
-      this.translate.use(type);// ar or en
-      
-    
-    }
+    this.slider = await this.apiService.getSlider();
+    this.services = (await this.apiService.getServices()).splice(0 , 2);
 
+    setTimeout(()=>{
+    this.startSlider()
+
+    } , 1500)
+    // this.contact = await this.apiService.contact();
+  };
+
+  Translate(type: string) {
+
+
+    this.translate.use(type);// ar or en
+
+
+  }
+
+ async  submit(){
+    if(this.contactForm.valid){
+      await this.apiService.contact(this.contactForm.value);
+      this.contactForm.patchValue({})
+
+      Swal.fire({
+        icon: 'success',
+  showConfirmButton: false,
+  timer: 1500
+      })
+
+    }
+  }
+  startSlider() {
+    
+    $('.home-slider').owlCarousel({
+      loop: true,
+      autoplay: true,
+      margin: 0,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn',
+      nav: true,
+      autoplayHoverPause: true,
+      items: 1,
+      dragTouch: false,
+      navText: ["<span class='ion-chevron-left'></span>", "<span class='ion-chevron-right'></span>"],
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        600: {
+          items: 1,
+          nav: false
+        },
+        1000: {
+          items: 1,
+          nav: true
+        }
+      }
+    });
+  }
 
 
 }
