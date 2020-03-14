370c67f649a6609fb3a99612e5d68a05f4fc34a4
Mohamed Abd ELmawgoud <49039674+MohamedAbdElmawgoud@users.noreply.github.com>
2020-03-14 20:50:18 +0200

    contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../apiServices/api.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'contact',
@@ -7,8 +10,15 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  contactForm = new FormGroup({
+    'name': new FormControl('', [Validators.required]),
+    'phone': new FormControl('', [Validators.required]),
+    "mail": new FormControl('', [Validators.required , Validators.email]),
+    "message": new FormControl('', [Validators.required]),
 
-  constructor(public translate:  TranslateService) { 
+
+  });
+  constructor(public translate:  TranslateService, private apiService: ApiService) { 
     const  currentLanguage  =  localStorage.getItem('lng') || 'en'
     this.Translate(currentLanguage) }
 
@@ -20,6 +30,20 @@ export class ContactComponent implements OnInit {
       this.translate.use(type);// ar or en
       
     
+    }
+
+    async  submit(){
+      if(this.contactForm.valid){
+        await this.apiService.contact(this.contactForm.value);
+        this.contactForm.patchValue({})
+  
+        Swal.fire({
+          icon: 'success',
+    showConfirmButton: false,
+    timer: 1500
+        })
+  
+      }
     }
 
 }
