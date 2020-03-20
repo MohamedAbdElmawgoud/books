321bbad47641bfda3a3a7bfaad5697b057e2a0fa
Mohamed Abd ELmawgoud <49039674+MohamedAbdElmawgoud@users.noreply.github.com>
2020-03-20 17:39:56 +0200

    reset Password

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -9,6 +9,7 @@ import Swal from 'sweetalert2';
   styleUrls: ['./forget-password.component.css']
 })
 export class ForgetPasswordComponent implements OnInit {
+  forget: any;
   forGetPassword = new FormGroup({
     email: new FormControl('', [
       Validators.required,
@@ -17,9 +18,39 @@ export class ForgetPasswordComponent implements OnInit {
    
     
    });
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
-  ngOnInit(): void {
+   ngOnInit() {
+    
   }
+  async submit(){
+    if (this.forGetPassword.valid) {
+      
+   try {
+     this.forget = await this.apiService.resetPassword(this.forGetPassword.value.email);
+    Swal.fire({
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 1500,
+      title: "check your mail"
+    }) 
+    console.log(this.forget);
+  } catch(e){
+    Swal.fire({
+      icon: 'error',
+      showConfirmButton: false,
+      timer: 1500
+    })  
+  }
+}
+else{
+  Swal.fire({
+    icon: 'error',
+    showConfirmButton: false,
+    timer: 1500,
+    title: "please enter valid mail"
+  })
+}
 
+  }
 }
