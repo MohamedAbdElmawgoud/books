cef859ae99ae9f5f4c31ed33b2f8993da1b45760
nader medhet <nadermedhet142@gmail.com>
2020-04-07 16:52:46 +0200

    change design for payment

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ApiService } from '../apiServices/api.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
+declare const goSell;
 @Component({
   selector: 'purchase',
   templateUrl: './purchase.component.html',
@@ -37,31 +38,46 @@ isAgree;
       return this.offer.amount  + (this.offer.amount * this.offer.bounce * 0.01)
     }
   async submit(){
-    if(this.isAgree){
-    try {
-      await this.apiService.Purchase({amount : 
-      this.offer.amount + (this.offer.amount * 0.001 * this.offer.bounce)
-      })
-      Swal.fire({
-        icon: 'success',
-        showConfirmButton: false,
-        timer: 1500
-      })  
-      }catch(e){
-        Swal.fire({
-          icon: 'error',
-          showConfirmButton: false,
-          timer: 1500
-        })  
-    }
-    }else{
-      Swal.fire({
-        icon: 'error',
-        showConfirmButton: false,
-        timer: 1500,
-        text : "you must agree for terms and polices"
-      })  
-    }
 
+
+    if(this.isAgree){
+      Swal.fire({
+        html : `
+        <iframe style="
+        width: 100%;
+        height: 600px;
+    "
+     src="https://q3seo.com/pay.html?amount=${this.willPay}&actAmount=${this.willGet}&token=${localStorage.getItem('token')}" frameborder="0"></iframe>
+        `,
+        showCloseButton: true
+
+      })
+  //   try {
+  //   //   await this.apiService.Purchase({amount : 
+  //   //   this.offer.amount + (this.offer.amount * 0.001 * this.offer.bounce)
+  //   //   })
+  //   //   Swal.fire({
+  //   //     icon: 'success',
+  //   //     showConfirmButton: false,
+  //   //     timer: 1500
+  //   //   })  
+  //   //   }catch(e){
+  //   //     Swal.fire({
+  //   //       icon: 'error',
+  //   //       showConfirmButton: false,
+  //   //       timer: 1500
+  //   //     })  
+  //   // }
+  //   // }else{
+  //   //   Swal.fire({
+  //   //     icon: 'error',
+  //   //     showConfirmButton: false,
+  //   //     timer: 1500,
+  //   //     text : "you must agree for terms and polices"
+  //   //   })  
+  //   // }
+
+  // }
+    }
   }
 }
